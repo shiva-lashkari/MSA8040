3f5df69f051f003bea0d293923cf0419f20cafe7
shiva-lashkari <67801622+shiva-lashkari@users.noreply.github.com>
2020-11-23 17:40:48 -0500

    First Crawler to read the main page
    Contain:
    1.  Title of the conference
    2. Ticker name of the company
    3.The detailed name of the company
    4. The date when the conference call script takes
    5. Category!!

---
+++ crawler.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+import requests
+
+import os
+import re
+import time
+import csv
+import random
+import requests
+# from selenium import webdriver
+from datetime import date, timedelta, datetime
+# with open("index.html") as page:
+#     soup = BeautifualSoup(page)
+url = "https://seekingalpha.com/earnings/earnings-call-transcripts"
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+page = requests.get(url, headers = headers)
+# soup = BeautifulSoup(page.text, 'html.parser')
+soup = BeautifulSoup(page.text, 'html5lib')
+attrs = {'class': 'list-group-item article'}
+posts = soup.find_all('li', attrs = attrs)
+
+counter = 1
+while (counter <= 1500):
+
+    for post in posts:
+        head = post.find('h3')
+        title = head.find('a', class_='dashboard-article-link').text
+        try:
+            desc = post.find('div', class_ = 'article-desc')
+            author = desc.find('a', recursive = False).text
+        except:
+            desc = 'None'
+            author = 'None'
+        try:
+            symbol = post.find('span', class_='article-symbols')
+            groupTitleTag = symbol.find('a')
+            groupTitle = groupTitleTag['title']
+            group = groupTitleTag.text
+        except:
+            groupTitleTag = 'None'
+            groupTitle = 'None'
+            group = 'None'
+        try:
+            # bullet = post.find('span', class_='bullet')
+            dateTemp = post.find('span', class_='bullet').find_next_sibling(string = True)
+            dateTemp = re.sub(r',|\.| +|\n', ' ', dateTemp).lstrip()
+            dateTemp = re.sub(' PM','PM',dateTemp).rstrip()
+            dateTemp = re.sub(' AM','AM',dateTemp)
+            if (dateTemp.count(' ') <7):
+                date = datetime.strptime(str(datetime.now().year) + ' ' + dateTemp, '%Y %a %b %d %I:%M%p')
+            else:
+                date = datetime.strptime(dateTemp, '%a %b %d %Y %I:%M%p')
+            # date = re.match(r'(\S)$',date)
+        except:
+            date = 'None'
+
+        post_line=[title,group,groupTitle,date,author]
+
+        with open('rawData.csv','a') as f:
+                try:
+                    writer=csv.writer(f)
+                    writer.writerow(post_line)
+                except UnicodeEncodeError:
+                    num_uncoded +=1
+        counter += 1
+
+    next_button = soup.find('li', class_='next')
+    if next_button:
+        next_page_link = next_button.find('a').attrs['href']
+        next_page = 'https://seekingalpha.com' + next_page_link
+        time.sleep(2)
+        page = requests.get(next_page, headers = headers)
+        soup = BeautifulSoup(page.text, 'html5lib')
+        posts = soup.find_all('li', attrs = attrs)
+    else:
+        break
