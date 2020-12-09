ed4b3cc4bfd1e622c6a1a69bb9ac3fd1b5c21e53
shiva-lashkari <lashkari.shiva@gmail.com>
2020-12-09 16:06:38 -0500

    Comments added to Code to be readable

--- crawler-innerPageCleaned.py
+++ crawler-innerPageCleaned.py
@@ -8,8 +8,6 @@ import time
 from datetime import date, timedelta, datetime
 from selenium import webdriver
 
-# timeout variable can be omitted, if you use specific value in the while condition
-# timeout = 9  # [seconds]
 
 browser = webdriver.Chrome(executable_path = '/home/shiva/myCodes/finalDM/chromedriver')
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
@@ -18,6 +16,7 @@ counter = 1
 timeout = 20
 failedURLs = []
 
+#This function compare paragraph text with the name of the participents to find their speech
 def searchResult(catParticipentTag, catParticipents):
     search_result = ""
     if catParticipentTag is not None:
@@ -49,7 +48,8 @@ def searchResult(catParticipentTag, catParticipents):
     return(search_result)
 
 
-#This function read each paragraph and seperate Title/Participents and speeches
+#This function read each paragraph : 1- Find the participents base on the argomans and seperate them
+#Also, find the speech for each person
 def readType(conference_id, conference_url, paragraph,comapnyParticipants, corporateParticipants):
     catParticipents = []
     catParticipentTag = paragraph.find_next_sibling('p')
@@ -62,13 +62,17 @@ def readType(conference_id, conference_url, paragraph,comapnyParticipants, corpo
             'Corporate Participants' not in catParticipents and 'Executives' not in catParticipents and \
             'Question-and-Answer' not in catParticipentTag.getText() and (time.time() < timeout_start + timeout):
 
+            #We are Checking the content of the paragraph with the participents name
             search_result = searchResult(catParticipentTag,catParticipents)
 
+            #if paragraph text not equal to participents name/operator, it means this paragraph is participent and we add it to participent list
             if catParticipentTag.getText().replace(" ","") != search_result and \
                 catParticipentTag.getText() != 'Operator':
                 catParticipents += ' / ' + catParticipentTag.getText()
                 catParticipentTag = catParticipentTag.find_next_sibling('p')
-
+            
+            #if paragraph text be equal to participents name/operator, it means next paragraph is contain the speech of this paragraphs participent
+            #  and we add it to speechs
             elif catParticipentTag is not None and (catParticipentTag.getText().replace(" ","") == search_result or \
                 catParticipentTag.getText() == 'Operator' or catParticipentTag.getText() == 'Presentation'):
                 
@@ -77,11 +81,12 @@ def readType(conference_id, conference_url, paragraph,comapnyParticipants, corpo
                 catParticipentTag = catParticipentTag.find_next_sibling('p')
                 speech = catParticipentTag.getText()
                 catParticipentTag = catParticipentTag.find_next_sibling('p')
-                # print(catParticipentTag)
+
+                # We stop it if the paragraph be equal to Question-and-Answer or Operator because in most of the cases the operator has speech at the end of the 
+                # conferance, we caugh this speech in line #161 then we can stop it in this point
                 while catParticipentTag is not None and catParticipentTag.getText().replace(" ","") != search_result \
                     and 'Question-and-Answer' not in catParticipentTag.getText() and 'Operator' not in catParticipentTag:
-                    # print('****')
-                    # print(catParticipentTag)
+
                     if catParticipentTag.getText().replace(" ","") != searchResult(catParticipentTag,catParticipents):
                         speech += ', ' + catParticipentTag.getText()
                         catParticipentTag = catParticipentTag.find_next_sibling('p')
@@ -91,8 +96,6 @@ def readType(conference_id, conference_url, paragraph,comapnyParticipants, corpo
                 
                 speech_line=[conference_id,conference_url, speechPerson, speech]
                 
-                # print(speech_line)
-                
                 try:
                     writer=csv.writer(f)
                     writer.writerow(speech_line)
@@ -100,7 +103,7 @@ def readType(conference_id, conference_url, paragraph,comapnyParticipants, corpo
                     num_uncoded +=1
         else:
             catParticipents += ' / ' + 'Operator'
-
+    #We need to delete the title paragraphs from our participents list
     if 'Operator' in catParticipents:
         catParticipents = catParticipents.replace(' / Operator','')
     if 'Operator' in catParticipents:
@@ -115,13 +118,13 @@ with open('rawData-test.csv', 'r') as file:
     for row in reader:
         timeout_start = time.time()
         participants = []
-        # while(len(participants) == 0):
-        conference_url = (row[-1])
         # conference_url = ("https://seekingalpha.com/article/4392903-idt-corporations-idt-ceo-shmuel-jonas-on-q1-2021-results-earnings-call-transcript")
+        conference_url = (row[-1])
         conference_id = (row[0])
         browser.get(conference_url)
         html = browser.page_source
-        innerSoup = BeautifulSoup(html, "lxml")
+        # innerSoup = BeautifulSoup(html, "lxml")
+        innerSoup = BeautifulSoup(html, 'html.parser')
         innerPostparagraphs = innerSoup.find_all('p')
         
         time.sleep(2)
@@ -137,8 +140,8 @@ with open('rawData-test.csv', 'r') as file:
             #Change QA boolean to True if page contain 'Question-and-Answer Session'
             if 'Question-and-Answer Session' in paragraph.getText():
                 QA = 1
-            if 'Presentation' in paragraph.getText():
-                Presentation = 1
+            # if 'Presentation' in paragraph.getText():
+            #     Presentation = 1
             # Run the readType base on the title 
             # Then we can seperate Company participents and Corporate participenta ans Call participetns and Executives
             if paragraph.getText() == 'Company Participants' or paragraph.getText() == 'Company Participants ':
@@ -173,7 +176,9 @@ with open('rawData-test.csv', 'r') as file:
             except UnicodeEncodeError:
                 num_uncoded +=1
         time.sleep(2)
-
+        
+        #if captcha stop the crawler it returns null for comapnyParticipants and corporateParticipants
+        #we stop the code if it happens more than 5 times
         if(len(comapnyParticipants) == 0 and len(corporateParticipants) == 0):
             failedURLs += conference_url 
             counter += 1
